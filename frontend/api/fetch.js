9528e09840c3a9fc1998ca5164de793c55ea9b89
Harish Nigam <harishnigam21@gmail.com>
2025-07-06 02:41:00 +0530

    separate frontend folder
    added api file for fetching

---
+++ frontend/api/fetch.js
@@ -0,0 +1,49 @@
+const fetch = async (to, method, id, obj) => {
+  const createResponse = (message) => {
+    const response = {
+      Message: message,
+    };
+    return response;
+  };
+  if (to && method) {
+    const url = `${process.env.REACT_APP_BACKEND_HOST}/${to}`;
+    if (method === "GET") {
+      try {
+        const response = await fetch(url, {
+          method: "GET",
+          headers: { "content-type": "application/json" },
+        });
+        return response;
+      } catch (error) {
+        return createResponse(error.message);
+      }
+    } else if (method === "POST") {
+      try {
+        const response = await fetch(url, {
+          method: "POST",
+          headers: { "content-type": "application/json" },
+          body: JSON.stringify(obj),
+        });
+        return response;
+      } catch (error) {
+        return createResponse(error.message);
+      }
+    } else if (method === "DELETE") {
+      try {
+        const response = await fetch(url, {
+          method: "DELETE",
+          headers: { "content-type": "application/json" },
+          body: JSON.stringify({ id: id }),
+        });
+        return response;
+      } catch (error) {
+        return createResponse(error.message);
+      }
+    }
+  } else {
+    return createResponse(
+      "Unable to fetch url, because target and method is missing"
+    );
+  }
+};
+module.exports = fetch;
